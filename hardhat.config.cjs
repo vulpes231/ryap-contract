b7f704027980cb78fd316ff8d109d8749572643d
Vulpes231 <whykayshot@gmail.com>
2024-11-26 19:24:08 +0100

    deploy v1

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,16 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const { vars } = require("hardhat/config");
-const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+require("dotenv").config();
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+// const SEPOLIA_PRIVATE_KEY =process.env("SEPOLIA_PRIVATE_KEY");
+const BSCTEST_PRIVATE_KEY = process.env.BSCTEST_PRIVATE_KEY;
+
+console.log("alchemy", BSCTEST_PRIVATE_KEY);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
   networks: {
-    sepolia: {
-      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+    // sepolia: {
+    //   url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    //   accounts: [SEPOLIA_PRIVATE_KEY],
+    // },
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [BSCTEST_PRIVATE_KEY],
     },
+    // bscmain: {
+    //   url: "https://bsc-dataseed.bnbchain.org/",
+    //   chainId: 56,
+    //   gasPrice: 20000000000,
+    //   accounts: [BSC_PRIVATE_KEY],
+    // },
   },
 };
--- ignition/modules/Yarntoken.js
+++ ignition/modules/Yarntoken.js
@@ -1,9 +1,9 @@
 // This setup uses Hardhat Ignition to manage smart contract deployments.
 // Learn more about it at https://hardhat.org/ignition
 
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-export default buildModule("YarntokenModule", (m) => {
+module.exports = buildModule("YarntokenModule", (m) => {
   const yarnToken = m.contract("Yarntoken", [
     "0xdac17f958d2ee523a2206206994597c13d831ec7",
   ]);
